69f1aa36abb7aa2263d92996d429faa6d351d6d2
Ryan Gay <ryan@revj.karoo.co.uk>
2012-09-08 23:10:40 +0100

    Too late for a message.
    Change-Id: I277dbde424696045a10aeb8b06e78ec1e3df6db1

--- products/skate.mk
+++ products/skate.mk
@@ -1,4 +1,4 @@
-# Inherit AOSP device configuration for skate.
+# Inherit device configuration
 $(call inherit-product, device/zte/skate/skate.mk)
 
 # Inherit common product files.
@@ -10,15 +10,13 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/skate
 
 # Setup device specific product configuration.
-PRODUCT_NAME := zte_skate
-PRODUCT_BRAND := ZTE
+PRODUCT_NAME := aokp_skate
 PRODUCT_DEVICE := skate
-PRODUCT_MODEL := Skate
+PRODUCT_BRAND := ZTE
 PRODUCT_MANUFACTURER := ZTE
+PRODUCT_MODEL := Skate
 
-
-#PRODUCT_PACKAGES += \
-#      Thinkfree
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=skate BUILD_ID=JRO03C BUILD_FINGERPRINT="ZTE/N880E_JB/atlas40:4.1.1/JRO03C/eng.songsy.20120718.233441:eng/test-keys" PRIVATE_BUILD_DESC="N880E_JB-eng 4.1.1 JRO03C eng.songsy.20120718.233441 test-keys" BUILD_NUMBER=eng.songsy.20120718.233441
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_480_800.zip:system/media/bootanimation.zip
