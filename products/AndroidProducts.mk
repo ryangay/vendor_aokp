56299fe3ae1f77e399d7dbf7b2a49c200401f062
Lalit Maganti <lalitmaganti@gmail.com>
2012-09-09 10:24:49 +0200

    Update products/AndroidProducts.mk
    Fixed crespo error

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,14 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/crespo.mk \
-    $(LOCAL_DIR)/crespo4g.mk \
-    $(LOCAL_DIR)/d2tmo.mk \
-    $(LOCAL_DIR)/grouper.mk \
-    $(LOCAL_DIR)/hercules.mk \
-    $(LOCAL_DIR)/maguro.mk \
-    $(LOCAL_DIR)/p5113.mk \
-    $(LOCAL_DIR)/p3113.mk \
-    $(LOCAL_DIR)/skate.mk \
-    $(LOCAL_DIR)/stingray.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/torospr.mk \
-    $(LOCAL_DIR)/wingray.mk
+    $(LOCAL_DIR)/skate.mk
